0154afb1e9d2aa6e0f76583de0ef5119c3290b9d
arthur <arthur.cavaile@gmail.com>
2018-01-17 17:32:09 +0100

    background

--- src/graphic/components/Background.java
+++ src/graphic/components/Background.java
@@ -3,6 +3,11 @@ package graphic.components;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import main.AngryBirds;
 
@@ -13,6 +18,14 @@ public class Background implements IComponents{
 		Dimension d = AngryBirds.GRAPHICCORE.getPreferredSize();
 		g.setColor(Color.WHITE);
 		g.fillRect(0, 0, (int) d.getWidth(), (int) d.getHeight());
-		
+		File file = new File("resources/images/table.jpg");
+		BufferedImage img;
+		try {
+			img = ImageIO.read(file);
+			g.drawImage(img, 0, (int) 0,(int) d.getWidth(),(int) d.getHeight(), null);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
