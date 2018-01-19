962d6dd9451697c56e524c51bcec25cf3bfb78eb
arthur <arthur.cavaile@gmail.com>
2018-01-19 17:31:14 +0100

    four

---
+++ src/bean/Oven.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package bean;
+
+import java.awt.Graphics2D;
+
+import graphic.components.ComponentsFactory;
+
+/**
+ * @author arthur
+ *
+ */
+public class Oven extends Animal {
+
+	public Oven() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Oven(double x, double y) {
+		super(x, y);
+		// TODO Auto-generated constructor stub
+	}
+	
+	/* (non-Javadoc)
+	 * @see graphic.components.IComponents#draw(java.awt.Graphics2D)
+	 */
+	@Override
+	public void draw(Graphics2D g) {
+		ComponentsFactory.getComponents("OVEN");		
+	}
+}
---
+++ src/graphic/components/OvenView.java
@@ -0,0 +1,31 @@
+package graphic.components;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import bean.Oven;
+import main.AngryBirds;
+
+public class OvenView implements IComponents {
+
+	@Override
+	public void draw(Graphics2D g) {
+		File file = new File ("resources/images/bird-oven.jpg");
+		BufferedImage img;
+		try {
+			img = ImageIO.read(file);
+			for(Oven oven : AngryBirds.GAMEMODE.getOvens()) {
+				g.drawImage(img, (int) oven.getPosX(), (int) oven.getPosY(), 40, 40, null);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
