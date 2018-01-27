b550f70f762448f1c1a4f8e0309df47010bbc659
DDOF <6102640+DDOF@users.noreply.github.com>
2018-01-27 17:59:45 +0100

    maven nature

--- src/logic/GameMode.java
+++ src/logic/GameMode.java
@@ -166,7 +166,6 @@ public class GameMode extends GameCore {
 			
 			if (Math.abs(round.getBird().getVelocityY()) < 0.15  && round.getBird().getPosY() > 460) {
 				setStatus(GameStatus.try_again);
-				System.out.println(round.getBird().getVelocityY());
 				round.setLives(round.getLives() - 1);
 				setMessage("Perdu : cliquez pour recommencer.");
 			}
